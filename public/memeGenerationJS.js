9a2c3be518958480bf1eb0ccdd9da6d0870b1534
Jigyasaa Sood <jsood@ucsd.edu>
2018-12-05 16:10:13 -0800

    added more meme templates, worked on upload a pic

--- public/memeGenerationJS.js
+++ public/memeGenerationJS.js
@@ -1,9 +1,50 @@
 var storage = firebase.storage();
 var storageRef = storage.ref();
-var testRef = storageRef.child('memeTemplates/SurprisedPikachu.JPG')
-var pikaRef = storageRef.child('memeTemplates/SurprisedPikachu.JPG')
-var safeRef = storageRef.child('memeTemplates/safe.jpg')
+var testRef = storageRef.child('memeTemplates/SurprisedPikachu.JPG');
+var pikaRef = storageRef.child('memeTemplates/SurprisedPikachu.JPG');
+var fwpRef = storageRef.child('memeTemplates/1990s-First-World-Problems.jpg');
+var aliensRef = storageRef.child('memeTemplates/Ancient-Aliens.jpg');
+var batmanRobinRef = storageRef.child('memeTemplates/Batman-Slapping-Robin.jpg');
+var blackGirlRef = storageRef.child('memeTemplates/Black-Girl-Wat.jpg');
+var blueBtnRef = storageRef.child('memeTemplates/BlueButton.JPG');
+var notMyBusinessRef = storageRef.child('memeTemplates/But-Thats-None-Of-My-Business.jpg');
+var bubbleGirlRef = storageRef.child('memeTemplates/Chubby-Bubbles-Girl.jpg');
+var conspiracyRef = storageRef.child('memeTemplates/Conspiracy-Keanu.jpg');
+var wonkaRef = storageRef.child('memeTemplates/Creepy-Condescending-Wonka.jpg');
+var dogeRef = storageRef.child('memeTemplates/Doge.jpg');
+var futuramaRef = storageRef.child('memeTemplates/Futurama-Fry.jpg');
+var catRef = storageRef.child('memeTemplates/Grumpy-Cat.jpg');
+var pillsRef = storageRef.child('memeTemplates/Hard-To-Swallow-Pills.jpg');
+var johnnyRef = storageRef.child('memeTemplates/Heres-Johnny.jpg');
+var haroldRef = storageRef.child('memeTemplates/Hide-the-Pain-Harold.jpg');
+var asianDadRef = storageRef.child('memeTemplates/High-Expectations-Asian-Father.jpg');
+var liveDangerouslyRef = storageRef.child('memeTemplates/I-Too-Like-To-Live-Dangerously.jpg');
+var interestingManRef = storageRef.child('memeTemplates/InterestingMan.jpg');
+var pigeonRef = storageRef.child('memeTemplates/Is-This-A-Pigeon.jpg');
+var jackieChanRef = storageRef.child('memeTemplates/Jackie-Chan-WTF.jpg');
+var leoRef = storageRef.child('memeTemplates/LeoDicaprioCheers.jpg');
+var spongebobRef = storageRef.child('memeTemplates/Mocking Spongebob.JPG');
+var offRampRef = storageRef.child('memeTemplates/OffRamp.jpg');
+var simplyRef = storageRef.child('memeTemplates/One-Does-Not-Simply.jpg');
+var oprahRef = storageRef.child('memeTemplates/Oprah-You-Get-A.jpg');
+var philosoraptorRef = storageRef.child('memeTemplates/Philosoraptor.jpg');
+var successKidRef = storageRef.child('memeTemplates/Success-Kid.jpg');
+var twoBtnsRef = storageRef.child('memeTemplates/Two-Buttons.jpg');
+var teacherRef = storageRef.child('memeTemplates/Unhelpful-High-School-Teacher.jpg');
+var skeletonRef = storageRef.child('memeTemplates/waiting-Skeleton.jpg');
+var xyRef = storageRef.child('memeTemplates/X-All-The-Y.jpg');
+var safeRef = storageRef.child('memeTemplates/safe.jpg');
+var distractedBoyfriendRef = storageRef.child('memeTemplates/Distracted Boyfriend.JPG');
 
+
+/*var selectedFile = document.getElementById('upload').files[0];
+if(selectedFile != null){
+   
+  var test = selectedFile.imageURL;
+  document.getElementById("test").src = test;
+
+}
+*/
 pikaRef.getDownloadURL().then(function(url){
   var test = url;
   document.getElementById("test").src = test;
@@ -22,6 +63,237 @@ safeRef.getDownloadURL().then(function(url){
         document.getElementById('test').src = test
     });
 })
+
+distractedBoyfriendRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("distractedBoyMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+fwpRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("FWPMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+aliensRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("AliensMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+batmanRobinRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("BatmanRobinMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+blackGirlRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("BlackGirlWatMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+blueBtnRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("BlueButtonMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+notMyBusinessRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("NotMyBusinessMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+bubbleGirlRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("bubblesGirlMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+conspiracyRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("conspiracyKeanuMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+wonkaRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("creepyWonkaMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+dogeRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("dogeMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+futuramaRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("futuramaMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+catRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("grumpyCatMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+pillsRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("pillsMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+johnnyRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("johnnyMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+haroldRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("haroldMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+asianDadRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("asianDadMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+liveDangerouslyRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("liveDangerouslyMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+interestingManRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("interestingManMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+pigeonRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("pigeonMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+jackieChanRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("jackieChanMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+leoRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("leoDicaprioMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+spongebobRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("mockingSpongebobMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+offRampRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("offRampMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+simplyRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("notSimplyMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+oprahRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("oprahMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+philosoraptorRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("philosoraptorMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+successKidRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("successKidMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+twoBtnsRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("twoButtonsMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+teacherRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("teacherMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+skeletonRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("skeletonMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+xyRef.getDownloadURL().then(function(url){
+  var test = url;
+  document.getElementById("xAllYMeme").addEventListener('click', () => {
+        document.getElementById('test').src = test
+    });
+})
+
+
+
+
+
+
+
 
 /* this is for the top/bot text and applying it to the image
 */
